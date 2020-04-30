c0c3f21e91872deb3c41db430e243f24ef3cdbe1
cyc4542015 <cyc4542000@gmail.com>
2020-04-29 21:30:45 -0600

    update script

--- bin/each_batch.sh
+++ bin/each_batch.sh
@@ -12,11 +12,11 @@ do
 
 ./main -i $1/BVZ-sawtooth0.max -g $2/BVZ-sawtooth0.sol -m ${mode[i]} >> ${mode[i]}.out
 ./main -i $1/BVZ-tsukuba0.max -g $2/BVZ-tsukuba0.sol -m ${mode[i]} >> ${mode[i]}.out
-./main -i $1/BVZ-venus0.max* -g $1/BVZ-venus0.sol -m ${mode[i]} >> ${mode[i]}.out
+./main -i $1/BVZ-venus0.max* -g $2/BVZ-venus0.sol -m ${mode[i]} >> ${mode[i]}.out
 
 ./main -i $1/KZ2-sawtooth0.max -g $2/KZ2-sawtooth0.sol -m ${mode[i]} >> ${mode[i]}.out
 ./main -i $1/KZ2-tsukuba0.max -g $2/KZ2-tsukuba0.sol -m ${mode[i]} >> ${mode[i]}.out
-./main -i $1/KZ2-venus0.max* -g $1/KZ2-venus0.sol -m ${mode[i]} >> ${mode[i]}.out
+./main -i $1/KZ2-venus0.max* -g $2/KZ2-venus0.sol -m ${mode[i]} >> ${mode[i]}.out
 
 done
 
